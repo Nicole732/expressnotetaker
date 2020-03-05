9155fc768b491806c81e46b9f3630920ff6c3ff3
Your Name Dorinetk <dketchiozo@gmail.com>
2020-03-04 22:15:25 -0500

    app.delete completed

--- server.js
+++ server.js
@@ -7,115 +7,101 @@ const data = require("./db/db.json")
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT =  process.env.PORT  || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// array in db
+// array in db.json
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+
+app.get("/", function (req, res) {
   
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
-    
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+app.get("/notes", function (req, res) {
+
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 
 // reads initial input in the JSON file and displays to client
 
-app.get("/api/notes", function(req, res) {
-//console.log("hit the GET for notes");
+app.get("/api/notes", function (req, res) {
+  //console.log("hit the GET for notes");
+  //console.log(data)
+  data[0].id = 1;
+  res.json(data)
 
-   
-    //console.log(data)
-    res.json(data)
+  
 });
 
 // get a new notes and pushes it to newData array
 
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function (req, res) {
 
+  var newData = req.body;
 
-    var newData = req.body;
+  console.log(newData);
 
-    console.log(newData);
+  // creates a unique id
+  const id = data[data.length - 1].id + 1
+  newData.id = id
 
-    // fs.readFile("./db/db.json","utf8",  (err, data) => {
+  // adds the newdata with its unique id to the data array
 
-    //     if (err) return console.log(err); //need to stop execution and keep server runing
+  data.push(newData);
 
-        // return res.json(data);
-        // creates a unique id
-        const id = data[data.length-1].id +1 
-        newData.id = id
+  //console.log(data);
 
-        ///
-        data.push(newData);
-        //console.log(data);
-
-        fs.writeFile("./db/db.json", JSON.stringify(data), "utf-8", err=>{
-            if (err) throw err
-            console.log("..writing")
-        })
-
-
-    // });
+  //adds the new data to the Json file after converting it to a string
+  fs.writeFile("./db/db.json", JSON.stringify(data), "utf-8", err => {
+    if (err) throw err
+    console.log("..writing")
+  })
 
   res.json(data);
 
 });
 
-app.get("/api/notes/:id",  function(req, res) {
+// deletes a note based on its id
 
-    console.log("start delete process...")
+app.delete("/api/notes/:id", function (req, res) {
 
-    var deleId = req.params.id;
+  console.log("started the delete process...")
 
-    console.log(deleId);
+  var deleteId = parseInt(req.params.id);
 
-    fs.readFile("./db/db.json","utf8", (err, data) => {
-        //loops through the array to see if there an entre with the id
+  // console.log("id of entry to be deleted: " + deleteId);
 
-        for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
 
-            //console.log(i);
-            if (deleId === data[i].id) {
+    // console.log(data[i].id);
 
-                const slicedData = data.splice(data[i],1);
-                
-                console.log(slicedData);
+    if (deleteId === data[i].id) {
 
-                fs.writeFile("./db/db.json", JSON.stringify(slicedData), "utf-8", err=>{
-                    if (err) throw err
-                    console.log("data deleted!!")
-                })
-            }
-        }
-        return res.send("No notes with this id" + id + "found");
-        if (err) throw err;
+      //console.log("DATA BEFORE:", data);
+      data.splice(i, 1);
+      console.log("data deleted!!")
+      //console.log("DATA AFTER:", data)
 
-        console.log("..deleting");
-    });
-    res.json(data);
-    console.log(data);
+    }
+    
+  }
+
+  res.json(data);
+
 });
-
-
-
-
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
